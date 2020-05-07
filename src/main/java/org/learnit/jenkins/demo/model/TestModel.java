47629072af0782f8443fbb78d5ccc73f108af0c8
Praveen Kumar S <uzumaki.praveen@gmail.com>
2020-05-07 09:50:45 +0530

    Add comment Test Model

--- src/main/java/org/learnit/jenkins/demo/model/TestModel.java
+++ src/main/java/org/learnit/jenkins/demo/model/TestModel.java
@@ -1,5 +1,9 @@
 package org.learnit.jenkins.demo.model;
 
+/*
+ * Test Model class is temporary employee class
+ * for jenkins demo project
+ */
 public class TestModel {
     private int id;
     private String name;
